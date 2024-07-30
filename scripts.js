893c52fe174fc876ba077614d7059dab3485bf47
cazoni007 <sebastiancazon301@gmail.com>
2024-07-30 13:04:52 -0400

    finishing the plans section with all styles

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-const rightArrow = document.querySelector('.table-container__arrow-right');
+const rightArrow = document.querySelector('.arrow--right');
 const container = document.querySelector('.table-container');
-const leftArrow = document.querySelector('.table-container__arrow-left');
+const leftArrow = document.querySelector('.arrow--left');
+
+const rightPlansArrow = document.querySelector('.arrow--plans-right');
+const plansContainer = document.querySelector('.plans__cards');
+const leftPlansArrow = document.querySelector('.arrow--plans-left');
+
+document.querySelector('.header__plans').addEventListener('click', function() {
+    document.querySelector('.plans').scrollIntoView({ behavior: 'smooth' });
+});
+
 rightArrow.addEventListener('click', moveRight);
 leftArrow.addEventListener('click', moveLeft);
 container.addEventListener('scroll', inactiveArrows);
+
 function moveRight(){
     container.scrollLeft += 10000;
 }
 function moveLeft(){
     container.scrollLeft -= 10000;
 }
+
 function inactiveArrows(){
     if (container.scrollLeft <= 0){
         leftArrow.classList.add('inactive');
@@ -24,3 +35,16 @@ function inactiveArrows(){
         leftArrow.classList.remove('inactive');
     }
 }
+
+rightPlansArrow.addEventListener('click', movePlansRight);
+leftPlansArrow.addEventListener('click', movePlansLeft);
+
+function movePlansRight(){
+    plansContainer.scrollLeft += plansContainer.offsetWidth / 3;
+}
+function movePlansLeft(){
+    plansContainer.scrollLeft -= plansContainer.offsetWidth / 3;
+}
+window.onload = function() {
+    plansContainer.scrollLeft = plansContainer.scrollWidth / 4;
+};
